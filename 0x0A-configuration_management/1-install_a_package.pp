c32d559bf5520a7b7356db7b1378c0528445af21
root <mshousha29@gmail.com>
2023-11-24 07:52:14 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# install flask from pip3
+# Ensure python3-pip package is installed
+package { 'python3-pip':
+  ensure => installed,
+}
 
+# Install flask from pip3
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
