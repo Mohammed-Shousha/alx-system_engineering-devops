fb40a13d64819c4c5f1960ccf97b281e59b3ef4f
mohammed shousha <mshousha29@gmail.com>
2024-01-02 03:43:25 -0800

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Returns to-do list information for a given employee ID."""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com"
+    id = sys.argv[1]
+
+    user = requests.get(f"{url}/users/{id}").json()
+    todos = requests.get(f"{url}/todos/?userId={id}").json()
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+
+    print(
+        f"Employee {user.get('name')} is done with tasks({len(completed)}/{len(todos)}):")
+
+    [print(f"\t {c}") for c in completed]
