82919a6c980489428121f5371f366921ecf27489
mohammed shousha <mshousha29@gmail.com>
2024-01-16 10:24:27 -0800

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0"
+    }
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 404:
+        return 0
+
+    results = response.json().get("data").get("subscribers")
+    return results
